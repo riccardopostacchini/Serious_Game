28d86055d0bf6b8290a08ee19f1b00e234f56030
riccardopostacchini <s1098289@studenti.univpm.it>
2024-07-10 16:17:13 +0200

    aggiunti altri dialoghi

--- Serious Game/Assets/Script/PuzzleTpScript.cs
+++ Serious Game/Assets/Script/PuzzleTpScript.cs
@@ -7,12 +7,14 @@ public class PuzzleTpScript : MonoBehaviour
     // Posizione di destinazione per il teletrasporto
     public Transform Destinazione;
     public GameObject Giocatore;
+    public GameObject TriggerDialogo;
 
     private void OnTriggerEnter(Collider other)
     {
         // Controlla se il collider appartiene al layer "Giocatore"
         if (other.gameObject.layer == LayerMask.NameToLayer("Giocatore"))
         {
+            TriggerDialogo.SetActive(true);
             // Teletrasporta il giocatore alla nuova posizione
             Giocatore.transform.position = Destinazione.position;
             Debug.Log("Giocatore teletrasportato alla nuova posizione.");
